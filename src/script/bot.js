bd3892d9c22fb579aaeee25eb5f416b115eac20e
prasan1h <prasanna.k.h.070@gmail.com>
2025-03-30 18:45:51 +0530

    website link integration done

--- src/script/bot.js
+++ src/script/bot.js
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 const TOKEN = process.env.TOKEN;
 const bot = new Telegraf(TOKEN);
 
-// const web_link = process.env.WEB_LINK;
+const web_link = process.env.WEB_LINK;
 
 bot.start((ctx) =>
   ctx.reply("Welcome :)))))", {
