a74bf5a69a4e401f530be4c2a3ecd9851dcc49f5
prasan1h <prasanna.k.h.070@gmail.com>
2025-03-30 15:31:13 +0530

    created cart file

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,44 @@ import { getData } from './db/db';
 const foods = getData();
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [cartItems, setCartItems] = useState(0);
+
+  const onAdd = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if(exist){
+      setCartItems(cartItems.map((x) => {
+        x.id === food.id ? {...exist,quantity : exist.quantity + 1} : x ;
+      }));
+    }
+    else{
+      setCartItems([...cartItems,{...food,quantity:1}]);
+    }
+  }; 
+
+  const onRemove = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist.quantity === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== food.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+        )
+      );
+    }
+  };
+
 
   return (
     <>
-      <div>
-        hello world
-        <Button title={"ADD"} disable={false} type={"add"} />
-        <Button title={"REMOVE"} disable={false} type={"remove"} />
-        <Button title={"CHECKOUT"} disable={false} type={"checkout"} />
-        {foods.map((food) => (
-          <Card key={food.id} food={food} />
-        ))}
+      <h1 className="heading">Order Food</h1>
+      <Cart cartItems={cartItems} onCheckout={onCheckout}/>
+      <div className="cards__container">
+        {foods.map((food) => {
+          return (
+            <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
+          );
+        })}
       </div>
     </>
   );
