582c5e186a08f303f0e2877ed9afe074963703d2
prasan1h <prasanna.k.h.070@gmail.com>
2025-03-30 13:38:42 +0530

    corrected data importing

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import './assets/style.css'
+import "./App.css";
+import "./assets/style.css";
 
-import Button from './components/button'
-import Card from './components/card'
+import Button from "./components/button";
+import Card from "./components/card";
 
-const {getData} = require('./db/db')
-const foods = getData()
+import { getData } from './db/db'; 
+
+const foods = getData();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         hello world
-        <Button title={"ADD"} disable={false} type={'add'}/>
-        <Button title={"REMOVE"} disable={false} type={'remove'}/>
-        <Button title={"CHECKOUT"} disable={false} type={'checkout'}/>
-        <Card/>
-        {foods.map(food = () =>{
-          return <Card key={id} food={food}/>;
-        })}
+        <Button title={"ADD"} disable={false} type={"add"} />
+        <Button title={"REMOVE"} disable={false} type={"remove"} />
+        <Button title={"CHECKOUT"} disable={false} type={"checkout"} />
+        {foods.map((food) => (
+          <Card key={food.id} food={food} />
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
