2d93411a525af30820f9139c73a36bff971ed649
prasan1h <prasanna.k.h.070@gmail.com>
2025-03-31 19:46:20 +0530

    added info command

--- src/script/bot.cjs
+++ src/script/bot.cjs
@@ -11,18 +11,20 @@ bot.start((ctx) =>{
   console.log(ctx.chat);
   const f_name = ctx.chat.first_name;
 
-  ctx.reply(`Welcome ${f_name} :)`, {
+  ctx.reply(`Welcome ${f_name} :) \n\n\ninformation : /info \n :-}`,
+  {
     reply_markup: {
-      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+      keyboard: [
+        [{ text: "tap to shop now", web_app: { url: web_link } }],
+    ],
     },
   })
 });
 
-bot.on(message('text'), async (ctx) => {
-  if(text != '/start'){
-  return ctx.reply('👍')}
+bot.command('info', (ctx) => ctx.reply('The E-commerce Telegram Bot (Mini App) is a chat-based shopping assistant that enables users to browse products, add items to their cart, and complete purchases—all within Telegram. Built using Telegraf.js for bot interactions and Node.js with Express for the backend, it integrates a web mini-app to provide a seamless UI for product selection and checkout. The bot fetches product data from MongoDB, processes orders, and can integrate payment gateways like Stripe or Razorpay for transactions. Users interact through text commands or inline buttons, while admins can manage inventory and orders via a backend panel. This lightweight solution is ideal for small businesses, offering a fast and intuitive e-commerce experience directly inside Telegram.'));
+
+bot.on(message("text"), async msg => {
+    await msg.reply("do not send message")
 });
-
-
 
 bot.launch();
