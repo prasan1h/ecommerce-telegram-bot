229033e6306537b088a88577277c5bee3982bc56
prasan1h <prasanna.k.h.070@gmail.com>
2025-03-31 18:47:32 +0530

    inserted username in chat

--- src/script/bot.cjs
+++ src/script/bot.cjs
@@ -1,25 +1,28 @@
 require("dotenv").config();
+
+const { message } = require('telegraf/filters')
 const { Telegraf } = require("telegraf");
-const TOKEN = process.env.TOKEN;
 
+const TOKEN = process.env.BOT_TOKEN;
 const bot = new Telegraf(TOKEN);
-
-if (!TOKEN) {
-  throw new Error(" Bot Token is missing! Set it in the .env file.");
-}
-
 const web_link = process.env.WEB_LINK;
 
-bot.start((ctx) =>
-  ctx.reply("Welcome :)))))", {
+bot.start((ctx) =>{
+  console.log(ctx.chat);
+  const f_name = ctx.chat.first_name;
+
+  ctx.reply(`Welcome ${f_name} :)`, {
     reply_markup: {
       keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
     },
   })
-);
+});
 
-// bot.start((ctx) => 
-//   ctx.reply("welcome")
-// );
+bot.on(message('text'), async (ctx) => {
+  if(text != '/start'){
+  return ctx.reply('👍')}
+});
+
+
 
 bot.launch();
