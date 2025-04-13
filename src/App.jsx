46ad1446f93424350b2f30d074f75fa1e28a3070
prasan1h <prasanna.k.h.070@gmail.com>
2025-04-13 12:27:44 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Cart from './components/cart';
 import { getData } from './db/db'; 
 
 const tele = window.Telegram.WebApp;
-tele.setResizeMode;
+// tele.setResizeMode;
 tele.setResizeMode('resize');
 
 // tele.expand();
