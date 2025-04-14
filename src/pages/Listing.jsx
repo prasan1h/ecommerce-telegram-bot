16c5fc95abc92d4fd8abc266ed5a93b031f6d6ea
prasan1h <prasanna.k.h.070@gmail.com>
2025-04-14 12:15:41 +0530

    check

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -43,18 +43,18 @@ const Listing = () => {
         }
       };
     
-    //   const onCheckout = () => {
-    //     tele.MainButton.text = "Checkout :)";
-    //     tele.MainButton.show();
-    //   };
+      const onCheckout = () => {
+        tele.MainButton.text = "Checkout :)";
+        tele.MainButton.show();
+      };
     
 
 
   return (
     <>
       <h1 className="heading">Order Food</h1>
-      {/* <Cart cartItems={cartItems} onCheckout={onCheckout}/> */}
-      <Cart cartItems={cartItems}/>
+      <Cart cartItems={cartItems} onCheckout={onCheckout}/>
+      {/* <Cart cartItems={cartItems}/> */}
       <div className="cards__container">
         {foods.map((food) => {
           return (
