a99acac76777669377122555e16e8489d12c90dc
prasan1h <prasanna.k.h.070@gmail.com>
2025-04-14 17:31:09 +0530

    listing updates

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -16,30 +16,28 @@ const Listing = () => {
       useEffect(() => {
         tele.ready();
         tele.expand();
-      });
-    
+      }, []);
+
+      
       const onAdd = (food) => {
         const exist = cartItems.find((x) => x.id === food.id);
-        if(exist){
-          setCartItems(cartItems.map((x) => 
-            x.id === food.id ? {...exist,quantity : exist.quantity + 1} : x 
+        if (exist) {
+          setCartItems(cartItems.map((x) =>
+            x.id === food.id ? { ...x, quantity: x.quantity + 1 } : x
           ));
+        } else {
+          setCartItems([...cartItems, { ...food, quantity: 1 }]);
         }
-        else{
-          setCartItems([...cartItems,{...food,quantity:1}]);
-        }
-      }; 
+      };
     
       const onRemove = (food) => {
         const exist = cartItems.find((x) => x.id === food.id);
         if (exist.quantity === 1) {
           setCartItems(cartItems.filter((x) => x.id !== food.id));
         } else {
-          setCartItems(
-            cartItems.map((x) =>
-              x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
-            )
-          );
+          setCartItems(cartItems.map((x) =>
+            x.id === food.id ? { ...x, quantity: x.quantity - 1 } : x
+          ));
         }
       };
     
@@ -51,18 +49,35 @@ const Listing = () => {
 
 
   return (
+    // <>
+    //   <h1 className="heading">Order Food</h1>
+    //   <Cart cartItems={cartItems} onCheckout={onCheckout}/>
+    //   {/* <Cart cartItems={cartItems}/> */}
+    //   <div className="cards__container">
+    //     {foods.map((food) => {
+    //       return (
+    //         <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
+    //       );
+    //     })}
+    //   </div>
+    // </>
+
     <>
-      <h1 className="heading">Order Food</h1>
-      <Cart cartItems={cartItems} onCheckout={onCheckout}/>
-      {/* <Cart cartItems={cartItems}/> */}
-      <div className="cards__container">
-        {foods.map((food) => {
-          return (
-            <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
-          );
-        })}
-      </div>
-    </>
+    <h1 className="heading">Order Food</h1>
+    <Cart cartItems={cartItems} onCheckout={onCheckout} />
+    <div className="cards__container">
+      {foods.map((food) => (
+        <Card
+          food={food}
+          key={food.id}
+          onAdd={onAdd}
+          onRemove={onRemove}
+        />
+      ))}
+    </div>
+  </>
+
+
   );
 }
 
