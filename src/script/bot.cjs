e8c914179dfd3e26e8a4a749ad9d160724050e4f
prasan1h <prasanna.k.h.070@gmail.com>
2025-04-15 21:27:56 +0530

    link correction

--- src/script/bot.cjs
+++ src/script/bot.cjs
@@ -58,7 +58,7 @@ bot.command('checkout', async (ctx) => {
   const username = encodeURIComponent(ctx.from.username || '');
   
   // Construct dynamic checkout URL
-  const checkoutUrl = `${WEB_LINK}/checkout?user=${userId}&name=${firstName}&username=${username}`;
+  const checkoutUrl = `${WEB_LINK}/checkout`;
 
   // Send a message and launch button
   await ctx.reply(
