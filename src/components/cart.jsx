78b9b24a3955bbe35ab200721f85d41387901d02
prasan1h <prasanna.k.h.070@gmail.com>
2025-04-16 20:49:54 +0530

    confirm 2 cart text

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -26,7 +26,7 @@ const cart = ({cartItems,onCheckout}) => {
         </div>
       )}
     <Button
-      title={`${cartItems.length === 0 ? "Order" : "Confirm!"}`}
+      title={`${cartItems.length === 0 ? "Order" : "Cart"}`}
       type={"checkout"}
       disable={cartItems.length === 0}
       onClick={onCheckout}
