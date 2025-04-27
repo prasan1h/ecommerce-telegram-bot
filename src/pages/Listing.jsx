2af9a585005453f57b8f5f2d55548c410a24815d
prasan1h <prasanna.k.h.070@gmail.com>
2025-04-27 11:48:55 +0530

    styling

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -196,7 +196,8 @@ const Listing = () => {
       )} */}
 
 
-      {/* Listing Page */}
+
+{/* Listing Page */}
 {step === 'listing' && (
   <>
     {userData?.first_name && (
@@ -208,11 +209,11 @@ const Listing = () => {
     
     <div className="cards__container">
       {Object.entries(foods).map(([categoryKey, categoryValue]) => (
-        <div key={categoryKey}>
+        <div key={categoryKey} className="category__block">
           {/* Category Title */}
-          <h2 className="category__heading">{categoryValue.title}</h2>
+          <h2 className="category__heading">{categoryValue.title}:</h2>
           
-          <div className="cards__inner">
+          <div className="cards__inner__wrap">
             {categoryValue.items.map((food, index) => (
               <Card
                 food={food}
@@ -228,6 +229,7 @@ const Listing = () => {
     </div>
   </>
 )}
+
 
 
 
