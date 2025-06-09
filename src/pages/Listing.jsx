d781c1ba66c96da13bcc2332f900d8951325c31f
prasan1h <prasanna.k.h.070@gmail.com>
2025-06-09 20:58:22 +0530

    list test

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,7 +8,7 @@ import Card from "../components/card";
 import Cart from "../components/cart";
 
 
-
+const allowedId = Number(process.env.REACT_APP_ALLOWED_TELEGRAM_ID);
 
 // const { userData } = require('../script/bot.cjs');
 import { getData } from "../db/db";
@@ -49,7 +49,7 @@ const Listing = () => {
   useEffect(() => {
     tele.ready();
     tele.expand();
-    const allowedId = Number(process.env.REACT_APP_ALLOWED_TELEGRAM_ID);
+    
   
     const user = window.Telegram.WebApp.initDataUnsafe.user;
     
