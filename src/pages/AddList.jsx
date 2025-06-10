b7b28e400d2a8b950d112e2ca261d96c11010cf7
prasan1h <prasanna.k.h.070@gmail.com>
2025-06-10 11:43:01 +0530

    url correction

--- src/pages/AddList.jsx
+++ src/pages/AddList.jsx
@@ -43,7 +43,7 @@ const AddList = () => {
   };
 
   try {
-    const response = await fetch(`${WEB_LINK}add`, {
+    const response = await fetch(`${RENDER_URL}/add`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
