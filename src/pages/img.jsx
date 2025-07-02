449343a82248ea13364097cbedae91f32b748449
prasan1h <prasanna.k.h.070@gmail.com>
2025-07-02 09:27:01 +0530

    images added

--- src/pages/img.jsx
+++ src/pages/img.jsx
@@ -4,20 +4,36 @@ import '../assets/style.css'
 import pizzaImg from '../assets/img/pizza.png';
 import burgerImg from '../assets/img/burger.png'
 import cocaImg from '../assets/img/cocacola.png'
-import saladImg from '../assets/img/salad.png'
-import waterImg from '../assets/img/water.png'
+import startersImg from '../assets/img/starters.png'
+import friedRiceImg from '../assets/img/friedrice.png'
 import iceCreamImg from '../assets/img/icecream.png'
-
+import frankieRollImg from '../assets/img/frankieroll.png'
+import snacksImg from '../assets/img/frenchfries.png'
+import soupImg from '../assets/img/hotsoups.png'
+import hotDrinkImg from '../assets/img/hotdrinks.png'
+import noodlesImg from '../assets/img/noodles.png'
+import sandwichImg from '../assets/img/sandwich.png'
+import juiceImg from '../assets/img/juice.png'
+import milkshakeImg from '../assets/img/milkshake.png'
+import pavBhajiImg from '../assets/img/pavbhaji.png'
 
   const images = {
   "Pizza" : pizzaImg,
   "Burger": burgerImg,
   "Cool Drinks": cocaImg,
-  "Hot Drinks": waterImg,
-  "Sandwichs": iceCreamImg,
-  "Snacks/Chaats": saladImg,
+  "Hot Drinks": hotDrinkImg,
+  "Sandwichs": sandwichImg,
+  "Snacks/Chaats": snacksImg,
   "Frankie Roll": iceCreamImg,
-  "Noodles": iceCreamImg,
+  "Noodles": noodlesImg,
+  "Frankie Roll": frankieRollImg,
+  "Hot Soups" : soupImg,
+  "Starters": startersImg,
+  "Fried Rice": friedRiceImg,
+  "Pav Bhajis": pavBhajiImg,
+  "Juice": juiceImg,
+  "Milkshake": milkshakeImg,
+  "Ice Cream": iceCreamImg
 };
 
 const img = ({
